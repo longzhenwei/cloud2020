495e7391d1f188759a59495008a80fdbf6997b75
longzhenwei <861745889@qq.com>
2020-09-28 22:29:56 +0800

    modify

--- cloud-config-center3344/src/main/java/com/zwlong/springcloud/ConfigCenterMain.java
+++ cloud-config-center3344/src/main/java/com/zwlong/springcloud/ConfigCenterMain.java
@@ -2,6 +2,7 @@ package com.zwlong.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigServer
 public class ConfigCenterMain {
     public static void main(String[] args) {
         SpringApplication.run(ConfigCenterMain.class,args);
